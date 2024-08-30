68194e32805dcf49fc28b831e749ceb8c550ea17
Bobesz <bob@bobnet.network>
2024-08-30 23:47:41 +0200

    QUAN-44 Set up project structure using Spring Boot

---
+++ src/main/java/network/bobnet/quantumgrid/email_service/EmailServiceApplication.java
@@ -0,0 +1,18 @@
+package network.bobnet.quantumgrid.email_service;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Generated
+@SpringBootApplication
+@EnableMongoRepositories
+public class EmailServiceApplication {
+
+	@Generated
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServiceApplication.class, args);
+	}
+
+}
